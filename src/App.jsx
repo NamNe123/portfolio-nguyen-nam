4bcf0a77d565c72944454a56d40415f9da460d28
NamNe123 <90250775+NamNe123@users.noreply.github.com>
2025-05-10 23:23:23 +0700

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
-import { DataProvider } from "./DataProvider";
+//import { DataProvider } from "./DataProvider";
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
   return (
-    <DataProvider>
+    //<DataProvider>
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
           <Hero />
         </div>
-        <StarsCanvas />
+        {/* <StarsCanvas /> */}
         <About />
         <Experience />
         <Tech />
@@ -19,11 +19,11 @@ const App = () => {
         {/* <Feedbacks /> */}
         <div className='relative z-0'>
           <Contact />
-          <StarsCanvas />
+          {/* <StarsCanvas /> */}
         </div>
       </div>
     </BrowserRouter>
-    </DataProvider>
+    //</DataProvider>
     
   );
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,8 +5,11 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
-
+  const computer = useGLTF(
+    import.meta.env.DEV
+      ? "./desktop_pc/scene.gltf"
+      : "/portfolio-nguyen-nam/desktop_pc/scene.gltf"
+  );
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
